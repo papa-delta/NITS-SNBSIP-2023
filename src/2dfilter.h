ae8b39272433c5384f7446d966fe0be5c2af47e0
papa-delta <125729402+papa-delta@users.noreply.github.com>
2023-07-10 10:25:54 +0530

    Code cleanup, removed unneeded commented-out code

--- src/2dfilter.h
+++ src/2dfilter.h
@@ -1,19 +1,3 @@
-//#include<stdio.h>
-//#include<stdlib.h>
-//#include"murmur.h"
-
-//int seed=98899;
-//int seed1=10007; 
-//int x=103,y=107,z=109; //diemnsion of the 2D filter.
-//int x=9973,y=10007;//,z=137;  //dimension of the 2D filter. A prime number is a candidate for good hashing.
-//int x=5647,y=6151;
-//int x2=1129,y2=1123;
-//int x2=179, y2=197;//275KB
-
-//static int x,y; - commented because redundant
-
-
-//#include "common.h"
 
 static int bits=61;
 
--- src/initCBF.h
+++ src/initCBF.h
@@ -1,34 +1,4 @@
 
-//All moved to common.h
-
-/*unsigned long int size=0;
-
-unsigned long int selectPrime(unsigned long int k)
-{
-	unsigned long int i;
-	for(i=1;i<total_prime;i++)
-	{
-		if(prime[i]>k)
-			return i;
-	}
-}
-
-double error(unsigned long int m, unsigned long int n)
-{
-	return pow((1-exp(-2*n/m)),2);
-}
-unsigned long int memory(unsigned long int n, double err)
-{
-	return (unsigned long int)(-(n*log(err))/pow(log(2),2));
-}
-unsigned long int number(unsigned long int m,double err)
-{
-	return (unsigned long int)(-(m*pow(log(2),2))/log(err));
-}*/
-
-
-//#include "common.h"
-
 void setDim(unsigned long int m)
 {
 	unsigned long int k=m/256; 
