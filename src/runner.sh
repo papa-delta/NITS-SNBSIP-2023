abdb6505802c87b7a1a9aab931abb48963079659
papa-delta <125729402+papa-delta@users.noreply.github.com>
2023-07-13 23:02:43 +0530

    Added runner script to run datasets automatically
    In this commit, I've added a runner.sh BASH scipt that compiles and runs the error correction code for each of the sequence files (in .txt format) in a given folder. The script takes in two parameters as input: the path to the error correction code and the path to the folder containing the dataset sequences. It runs the algorithm for each of the datasets, and stores the results obtained after each run in a separate "Results" folder. All output files are renamed appropriately in each run of the code to indicate which dataset they pertain to.

---
+++ src/runner.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Check if both path1 and path2 are provided as arguments
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 path1 path2"
+  exit 1
+fi
+
+# Store the arguments in variables
+path1=$1
+path2=$2
+
+mkdir ${path1}/Results
+
+# Compile KmerCo.c
+gcc "${path1}/KmerCo.c" -o "${path1}/KmerCo.out" -lm
+
+# Check if the compilation was successful
+if [ $? -ne 0 ]; then
+  echo "Compilation failed"
+  exit 1
+fi
+
+# Loop through each filename.txt in path2
+for file in "${path2}"/*.txt; do
+  # Extract the filename without the extension
+  filenamebase=$(basename "$file" .txt)
+  # Run KmerCo with the current filename.txt
+  ${path1}/KmerCo.out ${path2}/$filenamebase.txt
+	
+# Define an array of filenames
+filenames=("linecount" "Distinct" "Erroneous" "Trustworthy" "Result" "Found" "NotFound")
+
+# Loop through each filename in the array
+for filename in "${filenames[@]}"; do
+
+  # Rename the output file using the current filename
+  fnappend=`echo $filenamebase | cut -d_ -f2`
+  mv "${path1}/${filename}.txt" "${path1}/Results/${filename}_${fnappend}.txt"
+
+
+done
+done
+
+
+
